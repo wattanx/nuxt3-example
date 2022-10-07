bd821daff29b79a95803b9a885892c760b0b047e
wattanx <wattan.dev@gmail.com>
2022-10-07 15:06:43 +0900

    add dynamic routes

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+const id = ref("1");
+</script>
+
 <template>
-  <NuxtLink to="/about">about</NuxtLink>
+  <div>
+    <div>
+      <input v-model="id" />
+    </div>
+    <div>
+      <NuxtLink :href="`/slug/${id}`">to slug</NuxtLink>
+    </div>
+    <NuxtLin to="/about">ABOUT</NuxtLin>
+  </div>
 </template>
--- pages/slug/[id].vue
+++ pages/slug/[id].vue
@@ -1,6 +1,10 @@
+<script setup lang="ts">
+const { params } = useRoute();
+</script>
+
 <template>
   <div>
-    <div>about pageだよ</div>
+    <p>userid:{{ params.id }}</p>
     <div>
       <NuxtLink href="/">to home</NuxtLink>
     </div>
